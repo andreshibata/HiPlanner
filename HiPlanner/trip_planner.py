9ff7a4be3ff65b25d196f70a59c4ebd8dd5fd846
andreshibata <41885690+andreshibata@users.noreply.github.com>
2023-10-30 20:54:05 -0400

    Edits

--- HiPlanner/trip_planner.py
+++ HiPlanner/trip_planner.py
@@ -1,16 +1,20 @@
 import requests
+from datetime import datetime, timedelta
 
-API_KEY = ''
+API_KEY = "AIzaSyCGqvWL0jdBN8O1VrpMXImI5FWRq_YYjsQ"
 
-# Function to add time
+def get_travel_duration(start, end, mode="walking"):
+    endpoint = f"https://maps.googleapis.com/maps/api/directions/json?origin={start}&destination={end}&mode={mode}&key={API_KEY}"
+    response = requests.get(endpoint).json()
+    try:
+        duration = response['routes'][0]['legs'][0]['duration']['value']
+        return duration / 3600  # Convert seconds to hours
+    except IndexError:
+        print(f"Error: Could not get travel duration from {start} to {end}")
+        return 0
+
 def add_time(start_time, hours_to_add):
-    # Extracting AM or PM notation and converting 12-hour time to 24-hour format
-    if "PM" in start_time and not start_time.startswith('12'):
-        start_hour, start_minute = map(int, start_time.replace(" PM", "").split(":"))
-        start_hour += 12
-    else:
-        start_hour, start_minute = map(int, start_time.replace(" AM", "").replace(" PM", "").split(":"))
-
+    start_hour, start_minute = map(int, start_time.split(":"))
     end_hour = start_hour + int(hours_to_add)
     end_minute = start_minute + int((hours_to_add - int(hours_to_add)) * 60)
 
@@ -18,82 +22,92 @@ def add_time(start_time, hours_to_add):
         end_hour += 1
         end_minute -= 60
 
-    return f"{end_hour}:{end_minute:02d}"
+    if end_hour >= 24:
+        end_hour -= 24
 
-# Function to get travel duration using Google Maps Directions API assuming walking mode
-def get_travel_duration(origin, destination):
-    endpoint = f"https://maps.googleapis.com/maps/api/directions/json?origin={origin}&destination={destination}&mode=walking&key={API_KEY}"
+    return f"{end_hour:02d}:{end_minute:02d}"
+
+def calculate_schedule(start_time, end_time, start_position, end_position, attractions_with_time, lunch_cuisine, dinner_cuisine):
+    current_time = start_time
+    total_duration = 0
+
+    # Calculate travel durations and visit times at attractions
+    for attraction, time_at_attraction in attractions_with_time:
+        travel_duration = get_travel_duration(start_position, attraction)
+        total_duration += travel_duration + time_at_attraction
+        current_time = add_time(current_time, travel_duration)
+        print(f"Travel to {attraction}, arrive at {current_time}")
+        
+        current_time = add_time(current_time, time_at_attraction)
+        print(f"Spend {time_at_attraction} hours at {attraction}, leave at {current_time}")
+
+        start_position = attraction  # Update start position for next iteration
+
+    # Lunch
+    lunch_place = find_best_place(start_position, lunch_cuisine)
+    travel_duration = get_travel_duration(start_position, lunch_place)
+    current_time = add_time(current_time, travel_duration)
+    print(f"Have lunch at {lunch_place} at {current_time}")
+    total_duration += travel_duration + 1.5  # Assuming 1.5 hours for lunch
+    current_time = add_time(current_time, 1.5)
+
+    # Calculate durations for dinner and travel to end position
+    dinner_place = find_best_place(start_position, dinner_cuisine)
+    travel_duration_to_dinner = get_travel_duration(start_position, dinner_place)
+    current_time = add_time(current_time, travel_duration_to_dinner)
+    print(f"Have dinner at {dinner_place} at {current_time}")
+    total_duration += travel_duration_to_dinner + 1.5  # Assuming 1.5 hours for dinner
+    current_time = add_time(current_time, 1.5)
+
+    travel_duration_to_end = get_travel_duration(dinner_place, end_position)
+    current_time = add_time(current_time, travel_duration_to_end)
+    print(f"Travel to {end_position}, arrive at {current_time}")
+    total_duration += travel_duration_to_end
+
+    # Check if total duration exceeds available time
+    start_time_obj = datetime.strptime(start_time, "%H:%M")
+    end_time_obj = datetime.strptime(end_time, "%H:%M")
+    available_hours = (end_time_obj - start_time_obj).seconds / 3600
+
+    if total_duration > available_hours:
+        print("Warning: The entire trip will take more time than planned.")
+        print("Consider removing some attractions to fit the schedule.")
+        remove_attraction = input("Would you like to remove an attraction? (yes/no): ")
+        if remove_attraction.lower() == 'yes':
+            print("Available attractions:")
+            for i, (attraction, time_at_attraction) in enumerate(attractions_with_time, start=1):
+                print(f"{i}. {attraction}")
+            attraction_to_remove = int(input("Enter the number of the attraction to remove: "))
+            del attractions_with_time[attraction_to_remove - 1]
+            return calculate_schedule(start_time, end_time, start_position, end_position, attractions_with_time, lunch_cuisine, dinner_cuisine)
+
+    # If the trip fits the schedule or user chooses not to remove attractions
+    return attractions_with_time, total_duration
+
+def find_best_place(location, query):
+    endpoint = f"https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input={query}&inputtype=textquery&locationbias=point:{location}&key={API_KEY}&fields=formatted_address,name,rating"
     response = requests.get(endpoint).json()
-    duration = response['routes'][0]['legs'][0]['duration']['value'] / 3600  # Convert seconds to hours
-    return duration
+    if response['candidates']:
+        place = response['candidates'][0]
+        return f"{place['name']} ({place['formatted_address']}, Rating: {place.get('rating', 'N/A')})"
+    return "N/A"
 
-# Function to get latitude and longitude of a location using Google Maps Geocoding API
-def get_lat_lng(address):
-    endpoint = f"https://maps.googleapis.com/maps/api/geocode/json?address={address}&key={API_KEY}"
-    response = requests.get(endpoint).json()
-    location = response['results'][0]['geometry']['location']
-    return location['lat'], location['lng']
-
-# Update to the find_best_place function
-def find_best_place(bias_points, query, place_type):
-    best_rating = -1
-    best_place = None
-
-    for address in bias_points:
-        lat, lng = get_lat_lng(address)  # Get the latitude and longitude of the bias address
-        endpoint = f"https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input={query}&inputtype=textquery&locationbias=point:{lat},{lng}&key={API_KEY}&fields=formatted_address,name,rating"
-        response = requests.get(endpoint).json()
-        if response['candidates']:
-            place_address = response['candidates'][0]['formatted_address']
-            place_name = response['candidates'][0]['name']
-            place_rating = response['candidates'][0].get('rating', -1)
-
-            if place_rating > best_rating:
-                best_rating = place_rating
-                best_place = (place_name, place_address, place_rating)
-
-    return best_place
-def main():
-    # User input
-    START_TIME = input("Starting time (e.g., 9:00 AM): ")
-    END_TIME = input("End time (e.g., 8:00 PM): ")
-    START_POSITION = input("Starting position: ")
-    END_POSITION = input("End position: ")
-    ATTRACTION_1 = input("Attraction 1: ")
-    ATTRACTION_2 = input("Attraction 2: ")
-    LUNCH_CUISINE = input("Cuisine for lunch: ")
-    DINNER_CUISINE = input("Cuisine for dinner: ")
-
-    # Create Schedule
-    schedule = [{"position": START_POSITION, "time": START_TIME}]
-    
-    attraction_1_address = find_best_place([START_POSITION], ATTRACTION_1, "tourist_attraction")[1]  # Only need address for attractions
-    time_at_attraction_1 = add_time(schedule[-1]["time"], get_travel_duration(START_POSITION, attraction_1_address))
-    schedule.append({"position": attraction_1_address, "time": time_at_attraction_1})
-
-    potential_biases = [attraction_1_address, ATTRACTION_2]
-    lunch_place_name, lunch_place_address, lunch_place_rating = find_best_place(potential_biases, LUNCH_CUISINE, "restaurant")
-    time_for_lunch = add_time(time_at_attraction_1, 2)  # Assuming 2 hours at the attraction
-    schedule.append({"position": f"{lunch_place_name} ({lunch_place_rating} stars) - {lunch_place_address}", "time": time_for_lunch, "event": "Lunch"})
-
-    attraction_2_address = find_best_place([lunch_place_address], ATTRACTION_2, "tourist_attraction")[1]
-    time_at_attraction_2 = add_time(add_time(time_for_lunch, 1.5), get_travel_duration(lunch_place_address, attraction_2_address))  # 1.5 hours for lunch
-    schedule.append({"position": attraction_2_address, "time": time_at_attraction_2})
-
-    potential_biases = [attraction_2_address, END_POSITION]
-    dinner_place_name, dinner_place_address, dinner_place_rating = find_best_place(potential_biases, DINNER_CUISINE, "restaurant")
-    time_for_dinner = add_time(time_at_attraction_2, 2)  # Assuming 2 hours at the second attraction
-    schedule.append({"position": f"{dinner_place_name} ({dinner_place_rating} stars) - {dinner_place_address}", "time": time_for_dinner, "event": "Dinner"})
-
-    end_time = add_time(time_for_dinner, 1.5)  # Assuming 1.5 hours for dinner
-    schedule.append({"position": END_POSITION, "time": end_time})
-
-    # Print Schedule
-    for event in schedule:
-        if 'event' in event:
-            print(f"At {event['time']} have {event['event']} at {event['position']}")
-        else:
-            print(f"At {event['time']} be at {event['position']}")
+def get_user_input():
+    city = input("Enter the city you are traveling to: ")
+    start_time = input("Enter start time (HH:MM): ")
+    end_time = input("Enter end time (HH:MM): ")
+    start_position = input("Enter start position: ") + ", " + city
+    end_position = input("Enter end position: ") + ", " + city
+    num_attractions = int(input("Enter number of attractions: "))
+    attractions = []
+    for i in range(num_attractions):
+        attraction = input(f"Enter attraction {i+1}: ") + ", " + city
+        time_at_attraction = float(input(f"How much time (in hours) do you plan to spend at {attraction}? "))
+        attractions.append((attraction, time_at_attraction))
+    lunch_cuisine = input("Enter preferred cuisine for lunch: ") + " restaurant, " + city
+    dinner_cuisine = input("Enter preferred cuisine for dinner: ") + " restaurant, " + city
+    return start_time, end_time, start_position, end_position, attractions, lunch_cuisine, dinner_cuisine
 
 if __name__ == "__main__":
-    main()
+    start_time, end_time, start_position, end_position, attractions, lunch_cuisine, dinner_cuisine = get_user_input()
+    calculate_schedule(start_time, end_time, start_position, end_position, attractions, lunch_cuisine, dinner_cuisine)
